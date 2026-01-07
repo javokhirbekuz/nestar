c14818cd3db30e549a6b02b8cbf6c4c9ece32286
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2026-01-07 12:18:16 +0500

    fix: modify view module to record visits

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -4,9 +4,10 @@ import { MemberResolver } from './member.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import memberSchema from '../../schemas/Member.model';
 import { AuthModule } from '../auth/auth.module';
+import { ViewModule } from '../view/view.module';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: 'Member', schema: memberSchema }]), AuthModule],
+	imports: [MongooseModule.forFeature([{ name: 'Member', schema: memberSchema }]), AuthModule, ViewModule],
 	providers: [MemberService, MemberResolver],
 })
 export class MemberModule {}
