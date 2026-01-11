37ee1f33b6ebb9ecd09b86a0ea524725ec6c26a5
Javokhirbekuz <javoxirbek.uzb@gmail.com>
2026-01-11 22:35:00 +0500

    feat: develop createProperty GrahpQL api

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -9,5 +9,6 @@ import { ViewModule } from '../view/view.module';
 @Module({
 	imports: [MongooseModule.forFeature([{ name: 'Member', schema: memberSchema }]), AuthModule, ViewModule],
 	providers: [MemberService, MemberResolver],
+	exports: [MemberService],
 })
 export class MemberModule {}
